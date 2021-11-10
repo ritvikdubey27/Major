76b623f3366d4c097c3fe7420c9f5a4601a5c082
Ritvik Dubey <55030052+ritvikdubey27@users.noreply.github.com>
2021-11-10 09:48:34 +0530

    corrected all fetching process and bank name

--- src/main/java/com/example/readmsgtest/MainActivity.java
+++ src/main/java/com/example/readmsgtest/MainActivity.java
@@ -24,14 +24,19 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity {
     String msgData = "";
     ListView lv;
+    ListView lv1;
+    ListView lv2;
     ArrayList<String> lst=new ArrayList<>();
     ArrayAdapter<String>adapter;
+    ArrayList<String> lst1=new ArrayList<>();
+    ArrayAdapter<String>adapter1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lv=findViewById(R.id.msz);
+        lv1=findViewById(R.id.deep);
 
         if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_SMS}, 1);
@@ -45,7 +50,9 @@ public class MainActivity extends AppCompatActivity {
         ContentResolver cr = context.getContentResolver();
         Cursor c = cr.query(Telephony.Sms.CONTENT_URI, null, null, null, null);
         int totalSMS = 0;
+        int count =0;
         String s="";
+        String ban="";
         if (c != null) {
             totalSMS = c.getCount();
             if (c.moveToFirst()) {
@@ -65,11 +72,35 @@ public class MainActivity extends AppCompatActivity {
 
                                 if (body.contains("RS") || body.contains("Rs") || body.contains("rs") || body.contains("INR") || body.contains("Inr")) {
 
-                                    if (body.contains("A/C ") || body.contains("A/c ") || body.contains("Ac ") || body.contains("a/c ") || body.contains("ac ") || body.contains("A/C") || body.contains("A/c") || body.contains("Ac") || body.contains("a/c") || body.contains("ac") || body.contains("curr o/s")) {
+                                    if (body.contains("A/C ") || body.contains("A/c ") || body.contains("Ac ") || body.contains("a/c ") || body.contains("ac ") || body.contains("A/C") || body.contains("A/c") || body.contains(" Ac") || body.contains("a/c") || body.contains(" ac ") || body.contains("curr o/s")) {
 
                                         if (body.contains("credited ") || body.contains("debited ") || body.contains("Paid ") || body.contains("paid ") || body.contains("credited") || body.contains("debited") || body.contains("Avail") || body.contains("Avail ") || body.contains("spent") || body.contains("spent ")) {
                                             lst.add(dateFormat + number + body);
                                             s = s + dateFormat + number + body;
+
+                                            //bank ka naam print krega
+                                            if(number.contains("IDFC")) {
+                                                ban = ban + "IDFC Bank";
+                                            }
+                                            else if(number.contains("SBI")) {
+                                                ban = ban + "SBI Bank";
+                                            }
+                                            else if(number.contains("HDFC")) {
+                                                ban = ban + "HDFC Bank";
+                                            }
+                                            else if(number.contains("ICICI")) {
+                                                ban = ban + "ICICI Bank";
+                                            }
+                                            else {
+                                                ban = ban + "NULL Bank";
+                                            }
+                                            // bank ka naam wala khtm
+
+                                            //debit wale transactions
+                                            if (body.contains("debited") || body.contains(" debited ")) {
+                                                count++;
+                                                lst1.add("debited " + count);
+                                            }
                                         }
                                         //cred wale ka else
                                         else {
@@ -98,20 +129,20 @@ public class MainActivity extends AppCompatActivity {
                         default:
                             break;
                     }
-
-
                     c.moveToNext();
                 }
             }
-            Toast.makeText(context, ""+s, Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, ""+ban, Toast.LENGTH_LONG).show();
             c.close();
 
         } else {
             Toast.makeText(this, "No message to show!", Toast.LENGTH_SHORT).show();
         }
         adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, lst);
+        adapter1 = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, lst1);
 
         lv.setAdapter(adapter);
+        lv1.setAdapter(adapter1);
         return s;
     }
 
